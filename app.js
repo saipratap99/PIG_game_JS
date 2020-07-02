7e857b16228c6a6fb022c0325c9653f776cf16cc
saipratap99 <122005067@sastra.ac.in>
2020-07-02 13:09:00 +0530

    Declaring winner and then stop the functions of Roll dice and Hold

--- app.js
+++ app.js
@@ -8,33 +8,46 @@ GAME RULES:
 */
 
 // declare score vraibales and activePlayer variable
-var scores,currentScore,activePlayer,diceImg;
+var scores,currentScore,activePlayer,diceImg,playGame;
 diceImg = document.querySelector("img");
 init();
 
 // rolling dice event
 document.querySelector(".btn-roll").addEventListener('click',function(){
-    // Roll the dice
-    diceImg.style.display = "block";
-    var dice = Math.ceil(Math.random()*6);
-    diceImg.src = 'dice-'+dice+'.png';
-    if(dice !== 1){
-        // if dice value not 1
-        // add the dice value to current score
-        currentScore += dice;
-        document.querySelector('#current-'+activePlayer).textContent = currentScore;
-    }else{
-        nextPlayer();
+    if(playGame){
+        // Roll the dice
+        diceImg.style.display = "block";
+        var dice = Math.ceil(Math.random()*6);
+        diceImg.src = 'dice-'+dice+'.png';
+        if(dice !== 1){
+            // if dice value not 1
+            // add the dice value to current score
+            currentScore += dice;
+            document.querySelector('#current-'+activePlayer).textContent = currentScore;
+        }else{
+            nextPlayer();
+        }
     }
 });
 
 // Hold the current score and add to GLOBAL score
 document.querySelector(".btn-hold").addEventListener("click",function(){
-    // update the total score of current player and display it
-    scores[activePlayer] += currentScore;
-    document.getElementById('score-'+activePlayer).textContent = scores[activePlayer];
-    nextPlayer();
-})
+    if(playGame){
+        // update the total score of current player and display it
+        scores[activePlayer] += currentScore;
+        document.getElementById('score-'+activePlayer).textContent = scores[activePlayer];
+        // declare winner
+        if(scores[activePlayer] >= 20){
+            document.querySelector("#name-"+activePlayer).textContent = "WINNER!";
+            document.querySelector(".player-"+activePlayer+"-panel").classList.add("winner");
+            diceImg.style.display = "none";
+            playGame = false;
+        }
+        else{
+            nextPlayer();
+        }
+    }
+});
 
 // chnage the player alternatively
 function nextPlayer(){
@@ -60,6 +73,7 @@ function init(){
     document.querySelector("#score-1").textContent = 0;
     document.getElementById("current-0").textContent = 0;
     document.getElementById("current-1").textContent = 0;
+    playGame = true;
 }
 
 
